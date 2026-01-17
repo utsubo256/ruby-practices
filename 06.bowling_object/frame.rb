bcc56642822a49a5ad7f1b506aeaab465ee33163
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-01-17 18:00:47 +0900

    スコア計算ロジックの一部をGameクラスからFrameクラスに移動

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -4,24 +4,54 @@ require_relative 'shot'
 
 class Frame
   attr_reader :number, :first_shot, :second_shot
+  attr_accessor :next_frame
 
   def initialize(number, first_shot, second_shot = nil, third_shot = nil)
     @number = number
     @first_shot = Shot.new(first_shot)
     @second_shot = Shot.new(second_shot)
     @third_shot = Shot.new(third_shot)
+    @next_frame = nil
   end
 
   def score
-    [first_shot, second_shot, @third_shot].sum(&:score)
+    if last?
+      base_score
+    elsif strike?
+      strike_score
+    elsif spare?
+      spare_score
+    else
+      base_score
+    end
   end
 
   def strike?
     first_shot.score == 10
   end
 
+  def base_score
+    [first_shot, second_shot, @third_shot].sum(&:score)
+  end
+
+  private
+
   def spare?
     !strike? && [first_shot, second_shot].sum(&:score) == 10
+  end
+
+  def strike_score
+    if foundation?
+      10 + next_frame.first_shot.score + next_frame.second_shot.score
+    elsif next_frame.strike?
+      20 + next_frame.next_frame.first_shot.score
+    else
+      10 + next_frame.base_score
+    end
+  end
+
+  def spare_score
+    10 + next_frame.first_shot.score
   end
 
   def foundation?
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -4,42 +4,17 @@ require_relative 'frame'
 
 class Game
   def initialize(frame_scores)
-    @frames = frame_scores.each.with_index(1).map { |shots, i| Frame.new(i, *shots) }
+    @frames = frame_scores.map.with_index(1) { |shots, i| Frame.new(i, *shots) }
+    link_next_frames
   end
 
   def score
-    total_score = 0
-    (1..10).each { |frame_number| total_score += calculate_score(@frames[frame_number - 1]) }
-    total_score
+    @frames.sum(&:score)
   end
 
   private
 
-  def calculate_score(frame)
-    return frame.score if frame.last?
-
-    next_frame = @frames[frame.number]
-    if frame.strike?
-      strike_score(frame, next_frame)
-    elsif frame.spare?
-      spare_score(next_frame)
-    else
-      frame.score
-    end
-  end
-
-  def strike_score(frame, next_frame)
-    if frame.foundation?
-      10 + next_frame.first_shot.score + next_frame.second_shot.score
-    elsif next_frame.strike?
-      next_next_frame = @frames[frame.number + 1]
-      20 + next_next_frame.first_shot.score
-    else
-      10 + next_frame.score
-    end
-  end
-
-  def spare_score(next_frame)
-    10 + next_frame.first_shot.score
+  def link_next_frames
+    @frames.each_cons(2) { |frame, next_frame| frame.next_frame = next_frame }
   end
 end
